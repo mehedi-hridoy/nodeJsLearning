527f4ecb793df5a8811767bf2230dc8f39ff10e8
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2026-01-13 19:17:15 +0600

    basics of node js

--- file.js
+++ file.js
@@ -1,3 +1,22 @@
  const fs = require("fs");
 
- fs.writeFileSync("./test.txt", "Hello World from Node.js");
+
+//  Synchronous / blocking reques.....
+//  fs.writeFileSync("./test.txt", "Hello World from Node.js");
+
+
+//  // Async / non-blocking request.....
+//  fs.writeFile("./test.txt", "Hello World from Node.js", (err) => {
+//    if (err) throw err;
+//    console.log("File has been created");
+//  });
+
+ // read from file 
+ const result = fs.readFileSync("./contacts.txt", "utf-8");
+ console.log(result);
+
+
+
+ // append to a file
+ fs.appendFileSync("./contacts.txt", new Date().toISOString());
+ console.log("File has been updated");
