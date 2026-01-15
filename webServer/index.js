d88eed011c8905c9d5005b28b7f65a7f873fe9a6
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2026-01-15 17:01:10 +0600

    express

--- webServer/index.js
+++ webServer/index.js
@@ -1,9 +1,33 @@
-const http = require('http');
+// const http = require('http');
+// const fs = require('fs');
+// const url = require('url');
 
-const myServer = http.createServer((req,res) => {
-    res.end("Hello from Server!");
+// const myServer = http.createServer((req,res) => {
+//     res.end("Hello from Server!");
+// });
+
+// myServer.listen(8000,() => {
+//     console.log("Server is listening on port 8000");
+// }) 
+
+const http = require('http');
+const fs = require('fs');
+const url = require('url'); 
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+   return res.send('Hello from Express Server!');
 });
 
+app.get('/about', (req, res) => {
+    res.send('This is the About page.');
+});
+
+
+const myServer = http.createServer(app);
+
 myServer.listen(8000,() => {
-    console.log("Server is listening on port 8000");
-}) 
+    console.log("Express Server is listening on port 8000");
+})
